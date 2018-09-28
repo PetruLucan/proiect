471921cfd4bba2451b2e2109fe6db427c046a561
PetruLucan <lucanpetru11@gmail.com>
2018-09-28 22:17:49 +0300

    almost final

---
+++ main.js
@@ -0,0 +1,27 @@
+ 
+ $('.slider').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+
+        });
+
+// var showItemInfo = document.querySelectorAll(".product-display");
+// var itemInfoEl = document.getElementById("info-display");
+// console.log(itemInfoEl)
+// console.log(showItemInfo)
+
+
+// var timeout;
+// showItemInfo.forEach(el =>el.onmouseover = function(e) {
+//     timeout = setTimeout(function() {
+//         itemInfoEl.classList.remove(".hidden")
+//     }, 200)
+// });
+
+// showItemInfo.onmouseout = function(e) {
+//     if(timeout) {
+//         itemInfoEl.classList.add("hidden")
+//         clearTimeout(timeout);
+//     }
+// };
